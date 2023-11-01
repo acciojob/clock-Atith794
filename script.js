e9bf0dfc339201d332c8f558e798bd7b92ed911e
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-11-01 13:17:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function dat(){
+	const currentDate = new Date();
+	let year = currentDate.getFullYear();
+	let month = currentDate.getMonth()+1;
+	let day = currentDate.getDate();
+	let minutes = currentDate.getMinutes();
+	let hours = currentDate.getHours();
+	let seconds = currentDate.getSeconds();
+	let tim = document.getElementById("timer");
+	tim.textContent = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds}`;
+}
+
+dat();
+
+setInterval(dat, 1000);
+
+
